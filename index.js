fb2733f0bc0e9592035cc2e912e30f51b84894c0
blee241 <128437170+blee241@users.noreply.github.com>
2023-05-31 18:48:47 -0700

    added inquirer questions and prompt, completed prompt resolution with .then

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const questions = [''];
+const questions = [
+    'Enter up to three characters for your logo.',
+    'Enter a color keyword or a hexadecimal number to choose your text color.',
+    'Pick a shape for your logo.',
+    'Enter a color keyword or a hexadecimal number to choose your shape color.',
+];
 
 inquirer
     .prompt([
         {
-            type: 'list',
-            name: 'shape',
-            message: 'Which shape would you like in your logo?',
-            choices: ['Triangle', 'Circle', 'Square']
+            type: 'input',
+            message: questions[0],
+            name: 'text',
         },
-
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'text-color',
+        },
+        {
+            type: 'list',
+            message: questions[2],
+            choices: ['Triangle', 'Circle', 'Rectangle'],
+            name: 'shape',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'shape-color',
+        },
     ])
+    .then((res) => {
+        fs.writeFile('./generatedSVG/logo.svg', generateSVG(res), err =>
+        err ? console.log(err) : console.log('Generated logo.svg');
+        );
+    });
